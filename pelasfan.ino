cf6b375843bdca922c70a90d84b91f919ec89c92
jfrey <nomhad@gmail.com>
2016-05-03 19:17:26 +0200

    keeping only intervals

--- pelasfan.ino
+++ pelasfan.ino
@@ -5,16 +5,13 @@
 
 #define hallsensor 2 //The pin location of the fan PWM sensor (green wire on 4 pin fans)
 
+#define ledPin 13 // default LED
+
 // reduce fan speed
 // TODO: modulate according to actual RPM, especially when powered by more than 12v
 #define maxFanSpeed 200
 
 int fanSpeed = 0;
-
-// handling hallsensor
-volatile byte half_revolutions; // will be updated by interrupt
-unsigned int rpm;
-unsigned long timeold;
 
 // switch state
 unsigned long previousMillis = 0;
@@ -24,14 +21,7 @@ bool fanOn = true;
 
 void setup() {
   pinMode(fanSpdPin, OUTPUT);
-
-  //pinMode(hallsensor, OUTPUT);
-  //digitalWrite(hallsensor, HIGH);  //needs pull up enabled to be readable.
-  attachInterrupt(digitalPinToInterrupt(hallsensor), rpm_fun, RISING);  //for pin 2 - hall effect sensor on FAN
-  half_revolutions = 0;
-  rpm = 0;
-  timeold = 0;
-
+  
   Serial.begin(9600);
 }
 
@@ -39,22 +29,11 @@ void loop() {
 
   unsigned long currentMillis = millis();
 
-  //Get RPM readings
-  if (half_revolutions >= 20)
-  {
-    Serial.println("go");
-    //Update RPM every 20 counts, increase this for better RPM resolution,
-    //decrease for faster update
-    rpm = 30 * 1000 / (millis() - timeold) * half_revolutions;
-    timeold = millis();
-    half_revolutions = 0;
-    Serial.println(rpm, DEC);
-  }
 
   if (currentMillis - previousMillis >= interval)
   {
     Serial.println("interval");
-    // save the last time you blinked the LED
+    // save the last time state was switched
     previousMillis = currentMillis;
     fanOn = !fanOn;
     if (fanOn) {
@@ -71,9 +50,3 @@ void setSpeed(int fspeed) {
   analogWrite(fanSpdPin, fspeed);
 }
 
-void rpm_fun()
-{
-  half_revolutions++;
-  //Serial.println("up");
-  //Each rotation, this interrupt function is run twice
-}
