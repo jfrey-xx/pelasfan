1ff50ca870d4034d7fa35dce837a5357d73fd708
jfrey <nomhad@gmail.com>
2016-05-03 14:25:20 +0200

    enable debug, reduce max speed

--- pelasfan.ino
+++ pelasfan.ino
@@ -3,22 +3,29 @@
 
 #define hallsensor 2 //The pin location of the fan PWM sensor (green wire on 4 pin fans)
 
+// reduce fan speed
+// TODO: modulate according to actual RPM, especially when powered by more than 12v
+#define maxFanSpeed 200
 
-unsigned char fanSpeed = 0;
+int fanSpeed = 0;
 
 void setup() {
   pinMode(fanSpdPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   fanSpeed = fanSpeed + 10;
+  if (fanSpeed > 255) {
+    fanSpeed = 0;
+  }
   //setSpeed(fanSpeed);
   Serial.println("Current speed:");
   //Serial.println(fanSpeed);
   //delay(2000);
   setSpeed(0);
   delay(4000);
-  setSpeed(255);
+  setSpeed(maxFanSpeed);
   delay(4000);
 }
 
