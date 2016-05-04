2c3966c1c79dd39788e8821a450362b7b8378e8f
jfrey <nomhad@gmail.com>
2016-05-04 18:48:54 +0200

    debug

--- pelasfan.ino
+++ pelasfan.ino
@@ -60,6 +60,58 @@ ISR(WDT_vect)
   f_wdt++;
 }
 
+
+// https://github.com/lopenlab/atmega_power_save/blob/master/interrupt_power_save.ino
+
+/*
+   Prescale values:
+   0 2K (2048) cycles 16 ms
+   1 4K (4096) cycles 32 ms
+   2 8K (8192) cycles 64 ms
+   3 16K (16384) cycles 0.125 s
+   4 32K (32768) cycles 0.25 s
+   5 64K (65536) cycles 0.5 s
+   6 128K (131072) cycles 1.0 s
+   7 256K (262144) cycles 2.0 s
+   8 512K (524288) cycles 4.0 s
+   9 1024K (1048576) cycles 8.0 s
+   Other - reserved.
+*/
+
+/* function originates in the CitizenWatt project */
+void watchdog_setup(uint8_t prescale)
+{
+  prescale = min(9, prescale);
+
+  /* 3 least significant bits of prescalar go to WDTCSR firectly,
+     first bit goes to WDP3 */
+  uint8_t wdtcsr = prescale & ((1 << 0) | (1 << 1) | (1 << 2));
+  if (prescale & (1 << 3))
+    wdtcsr |= _BV(WDP3);
+
+  /* allow watchdog configuration change */
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+  /* Set prescale */
+  WDTCSR = wdtcsr;
+}
+
+void watchdog_int_enable()
+{
+  uint8_t wdtcsr = WDTCSR;
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+  /* clear the WD interrupt enable bit */
+  WDTCSR = wdtcsr | _BV(WDIE);
+
+}
+
+void watchdog_int_disable()
+{
+  uint8_t wdtcsr = WDTCSR;
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+  /* clear the WD interrupt enable bit */
+  WDTCSR = wdtcsr & ~_BV(WDIE);
+}
+
 void setup() {
 
   // will use bounce of LOW state to maintain fan on
@@ -76,20 +128,9 @@ void setup() {
     }
   }
 
-
-  /*** Setup the WDT ***/
-  /* Clear the reset flag. */
-  MCUSR &= ~(1 << WDRF);
-  /* In order to change WDE or the prescaler, we need to
-     set WDCE (This will allow updates for 4 clock cycles).
-  */
-  WDTCSR |= (1 << WDCE) | (1 << WDE);
-  /* set new watchdog timeout prescaler value */
-  WDTCSR = 1 << WDP0 | 1 << WDP3; /* 8.0 seconds */
-  // WDTCSR = 1 << WDP3; /* 4.0 seconds */
-  // WDTCSR = 1 << WDP1 | 1 << WDP2; /* 1.0 seconds */
-  /* Enable the WD interrupt (note no reset). */
-  WDTCSR |= _BV(WDIE);
+  /* Watchdog consumed about 5 uA. Comment out to get down to ~0.5 uA*/
+  watchdog_setup(7);
+  watchdog_int_enable();
 
   // disable everything not needed
   power_adc_disable(); // ADC converter
@@ -103,6 +144,8 @@ void setup() {
   if (!debug) {
     power_usart0_disable();// Serial (USART)
   }
+
+  debugMsg("setup done");
 }
 
 void debugMsg(String msg) {
@@ -117,15 +160,18 @@ void sleepNow()  {
   debugMsg("go to sleep");
 
   // if going sleep on ON phase, need PWM
-  if (cycleOn) {
-    debugMsg("light sleep");
-    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-  }
-  // otherwise, can go really deep
-  else {
-    debugMsg("deep sleep");
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  }
+  //  if (cycleOn) {
+  //    debugMsg("light sleep");
+  //    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+  //  }
+  //  // otherwise, can go really deep
+  //  else {
+  //    debugMsg("deep sleep");
+  //    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  //  }
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
   sleep_enable();
 
   interrupts();
@@ -133,7 +179,8 @@ void sleepNow()  {
 
   // go to sleep
   asleep = true;
-  sleep_mode();
+  //sleep_mode();
+  sleep_cpu();
   // awake
   asleep = false;
 
@@ -197,7 +244,7 @@ void checkTimer() {
       debugMsg("-- change fan");
     }
     debugMsg("periodic mode, go to sleep");
-    sleepNow();
+    //sleepNow();
     debugMsg("after sleep in checkTimer");
   }
 
